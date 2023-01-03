f735d1eadf10f7d422eacbf000c86e9663aef803
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-03 14:27:06 +0100

    modif firTree

--- firTree.js
+++ firTree.js
@@ -1,8 +1,13 @@
-function firTree(size) {
-  for (let x = 0; x < size; x++) {
-    for (let y = 0; y < x; y++) {
-      console.log("*");
+function firTree(rows) {
+  for (let i = 0; i < rows; i++) {
+    let line = "";
+    for (let j = 0; j < rows - i - 1; j++) {
+      line += " ";
     }
+    for (let k = 0; k <= 2 * i; k++) {
+      line += "*";
+    }
+    console.log(line);
   }
 }
 
