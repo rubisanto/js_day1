b37ddbaa1786b811ca408ea65c3e0fff811ec9a3
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-04 11:33:55 +0100

    modif 18

--- 18_mySortWordArray.js
+++ 18_mySortWordArray.js
@@ -1,8 +1,7 @@
 function mySortWordArray(string) {
+  let array = [];
   // en ternaire retourner tableau vide si string vide
-  if (string === "") {
-    array = [];
-  } else {
+  if (string !== "") {
     array = string.split(" ");
     array.sort();
     return array;
