db97a3826fc01f2cc00c07cf48e392ba95875022
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-05 16:44:31 +0100

    modif 31

--- 31_myCalc.js
+++ 31_myCalc.js
@@ -6,7 +6,27 @@ class Calc {
 
   compute(operation) {
     const elements = operation.split(" ");
+    // si y a un opérateur faire une fonction spécifique
+    const operators = elements.filter((element) =>
+      ["+", "-", "/", "*"].includes(element)
+    );
 
+    operators.forEach((element, index) => {
+      switch (element) {
+        case "+":
+          // Do something specific for the + operator
+          break;
+        case "-":
+          // Do something specific for the - operator
+          break;
+        case "/":
+          // Do something specific for the / operator
+          break;
+        case "*":
+          // Do something specific for the * operator
+          break;
+      }
+    });
     // this.result = eval(operation);
     this.history.push(this.result);
     return `${operation} = ${this.result}`;
