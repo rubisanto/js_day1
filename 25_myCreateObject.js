77306ee8e7dd3868aefa80f6a69ed452e8f43f4d
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-05 10:27:58 +0100

    add 25

---
+++ 25_myCreateObject.js
@@ -0,0 +1,9 @@
+function myCreateObject(surname, name, age) {
+  var obj = {};
+  obj.surname = surname;
+  obj.name = name;
+  obj.age = age;
+  return obj;
+}
+
+module.exports = myCreateObject;
--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const main = require("./1_Example.js");
-const main2 = require("./2_InputArgs.js");
-const myEvilPow = require("./13_myEvilPow.js");
-const firTree = require("./firTree.js");
-const mySortWordArray = require("./18_mySortWordArray.js");
+const myCreateObject = require("./25_myCreateObject");
 
-console.log(main());
-console.log(main2(1, 2));
-console.log(myEvilPow(5, 2));
-firTree(3);
-console.log(mySortWordArray(""));
+console.log(myCreateObject("Doe", "John", 35));
