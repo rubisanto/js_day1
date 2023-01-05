1a278279a3bff7a039c9a6813023c815c155985c
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-05 15:25:35 +0100

    add 31

---
+++ 31_myCalc.js
@@ -0,0 +1,22 @@
+class Calc {
+  constructor() {
+    this.result = 0;
+    this.history = [];
+  }
+
+  compute(operation) {
+    this.result = eval(operation);
+    this.history.push(this.result);
+    return `${operation} = ${this.result}`;
+  }
+
+  lastResult() {
+    return this.result;
+  }
+
+  history() {
+    return this.history;
+  }
+}
+
+module.exports = Calc;
