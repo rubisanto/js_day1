31f79d60d3eb0058d1afb47d1e9e4e6a04e2e86b
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-05 15:06:16 +0100

    modif 30

--- 30_myObjectValues.js
+++ 30_myObjectValues.js
@@ -1,6 +1,12 @@
 function myObjectValues(obj) {
   return (
     Object.keys(obj)
+      .filter(
+        (key) =>
+          typeof obj[key] !== "function" &&
+          !Array.isArray(obj[key]) &&
+          typeof obj[key] !== "object"
+      )
       .map((key) => `${key}: ${obj[key]}`)
       // le join permet de renvoyer un string
       .join(", ")
