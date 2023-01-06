f349135bc24cb626a8bc0e93b5746f0ccb220b90
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-06 13:47:30 +0100

    modif commentaire 30

--- 30_myObjectValues.js
+++ 30_myObjectValues.js
@@ -1,14 +1,18 @@
 function myObjectValues(obj) {
+  // Retourner une chaîne de caractères contenant les valeurs des propriétés de l'objet qui ne sont pas des fonctions, des tableaux ou des objets
   return (
+    // Récupérer les noms des propriétés de l'objet dans un tableau
     Object.keys(obj)
+      // Filtrer le tableau en ne conservant que les propriétés du type souhaité
       .filter(
         (key) =>
           typeof obj[key] !== "function" &&
           !Array.isArray(obj[key]) &&
           typeof obj[key] !== "object"
       )
+      // Transformer chaque élément du tableau en une chaîne de la forme "nom_de_la_propriété: valeur_de_la_propriété"
       .map((key) => `${key}: ${obj[key]}`)
-      // le join permet de renvoyer un string
+      // Concaténer les chaînes de caractères en une seule séparée par des virgules et des espaces
       .join(", ")
   );
 }
